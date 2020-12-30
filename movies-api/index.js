fabd1cf5639e20805dc8ef53efbd261a7e93fa4b
Crotshot <46968070+Crotshot@users.noreply.github.com>
2020-12-30 15:18:24 +0000

    Added routing for add, update, delete movie

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import bodyParser from 'body-parser';
 import moviesRouter from './api/movies';
+
 
 dotenv.config();
 
@@ -8,6 +10,10 @@ const app = express();
 
 // eslint-disable-next-line no-undef
 const port = process.env.PORT;
+
+//configure body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 app.use(express.static('public'));
 app.use('/api/movies', moviesRouter);
