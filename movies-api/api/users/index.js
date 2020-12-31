00e1e653bab1a38e985bf39ccc98d82ba5429ddf
Crotshot <46968070+Crotshot@users.noreply.github.com>
2020-12-31 16:42:53 +0000

    Week 9 user Favorites

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -9,8 +9,9 @@ router.get('/', (req, res) => {
 });
 
 // register
-router.post('/', (req, res) => {
-        new User(req.body).save().then(user => res.status(200).json({success:true,token:"FakeTokenForNow"}))
+router.post('/', (req, res ,next) => {
+    // eslint-disable-next-line no-unused-vars
+    User.create(req.body).then(user => res.status(200).json({success:true,token:"FakeTokenForNow"})).catch(next);
 });
 
 // Update a user
@@ -23,4 +24,29 @@ router.put('/:id',  (req, res) => {
     })
     .then(user => res.json(200, user));
 });
+
+router.post('/:userName/favourites', (req, res, next) => {
+    const newFavourite = req.body;
+    const query = {username: req.params.userName};
+    if (newFavourite && newFavourite.id) {
+      User.find(query).then(
+        user => {
+          (user.favourites)?user.favourites.push(newFavourite):user.favourites =[newFavourite];
+          User.findOneAndUpdate(query, {favourites:user.favourites}, {
+            new: true
+          }).then(user => res.status(201).send(user));
+        }
+      ).catch(next);
+    } else {
+        res.status(401).send("Unable to find user")
+    }
+  });
+
+  router.get('/:userName/favourites', (req, res, next) => {
+    const user = req.params.userName;
+    User.find( {username: user}).then(
+        user => res.status(201).send(user.favourites)
+    ).catch(next);
+  });
+  
 export default router;
